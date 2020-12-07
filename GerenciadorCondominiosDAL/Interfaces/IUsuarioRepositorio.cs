57ce255c32c0551bc250b5c02a18aff31f456a48
Eduardo Teixeira <eduardo@datamob.com.br>
2020-12-07 18:27:14 -0300

    Criacao do repositorio generico e do repositorio de usuarios

---
+++ GerenciadorCondominiosDAL/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciadorCondominiosDAL.Interfaces
+{
+    interface IUsuarioRepositorio
+    {
+    }
+}
---
+++ GerenciadorCondominiosDAL/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,122 @@
+﻿using GerenciadorCondominiosDAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominiosDAL.Repositorios
+{
+    public class RepositorioGenerico<TEntity> : IRepositorioGenerico<TEntity> where TEntity : class
+    {
+        private readonly Contexto Contexto;
+
+        public RepositorioGenerico(Contexto contexto)
+        {
+            Contexto = contexto;
+        }
+
+        public async Task Atualizar(TEntity entity)
+        {
+            try
+            {
+                Contexto.Set<TEntity>().Update(entity);
+                await Contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(TEntity entity)
+        {
+            try
+            {
+                Contexto.Set<TEntity>().Remove(entity);
+                await Contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(int id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                Contexto.Set<TEntity>().Remove(entity);
+                await Contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Excluir(string id)
+        {
+            try
+            {
+                var entity = await PegarPeloId(id);
+                Contexto.Set<TEntity>().Remove(entity);
+                await Contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Inserir(TEntity entity)
+        {
+            try
+            {
+                await Contexto.AddAsync(entity);
+                await Contexto.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(int id)
+        {
+            try
+            {
+                return await Contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<TEntity> PegarPeloId(string id)
+        {
+            try
+            {
+                return await Contexto.Set<TEntity>().FindAsync(id);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> PegarTodos()
+        {
+            try
+            {
+                return await Contexto.Set<TEntity>().ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
