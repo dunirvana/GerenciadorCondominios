38347c7baa3d8189cd08689bb53689e43ed3dcad
Eduardo Teixeira <eduardo@datamob.com.br>
2020-12-07 18:36:54 -0300

    Criacao de rotinas de manipulacao dos usuarios

--- GerenciadorCondominiosDAL/Interfaces/IUsuarioRepositorio.cs
+++ GerenciadorCondominiosDAL/Interfaces/IUsuarioRepositorio.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using GerenciadorCondominiosBLL.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominiosDAL.Interfaces
 {
-    interface IUsuarioRepositorio
+    public interface IUsuarioRepositorio : IRepositorioGenerico<Usuario>
     {
+        int VerificarSeExisteRegistro();
+        
+        Task LogarUsuario(Usuario usuario, bool lembrar);
+
+        Task<IdentityResult> CriarUsuario(Usuario usuario, string senha);
+
+        Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao);
     }
 }
--- GerenciadorCondominiosDAL/Repositorios/UsuarioRepositorio.cs
+++ GerenciadorCondominiosDAL/Repositorios/UsuarioRepositorio.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using GerenciadorCondominiosBLL.Models;
+using GerenciadorCondominiosDAL.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GerenciadorCondominiosDAL.Repositorios
 {
-    class UsuarioRepositorio
+    public class UsuarioRepositorio : RepositorioGenerico<Usuario>, IUsuarioRepositorio
     {
+        private readonly Contexto Contexto;
+        private readonly UserManager<Usuario> GerenciadorUsuarios;
+        private readonly SignInManager<Usuario> GerenciadorLogin;
+
+        public UsuarioRepositorio(Contexto contexto, UserManager<Usuario> gerenciadorUsuarios, SignInManager<Usuario> gerenciadorLogin) : base(contexto)
+        {
+            Contexto = contexto;
+            GerenciadorUsuarios = gerenciadorUsuarios;
+            GerenciadorLogin = gerenciadorLogin;
+        }
+
+        public Task<IdentityResult> CriarUsuario(Usuario usuario, string senha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IncluirUsuarioEmFuncao(Usuario usuario, string funcao)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task LogarUsuario(Usuario usuario, bool lembrar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int VerificarSeExisteRegistro()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
