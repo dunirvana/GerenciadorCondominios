55e5a66c99f8aba62d89e9d22045a2362e321a02
Eduardo Teixeira <eduardo@datamob.com.br>
2021-01-11 21:48:56 -0300

    Dashboard - 5

---
+++ GerenciadorCondominios/ViewComponents/UltimasMovimentacoesViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using GerenciadorCondominiosDAL.Interfaces;
+using GerenciadorCondominiosDAL.Repositorios;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.ViewComponents
+{
+    public class UltimasMovimentacoesViewComponent : ViewComponent
+    {
+        private readonly IHistoricoRecursoRepositorio HistoricoRecursosRepositorio;
+
+        public UltimasMovimentacoesViewComponent(IHistoricoRecursoRepositorio historicoRecursosRepositorio)
+        {
+            HistoricoRecursosRepositorio = historicoRecursosRepositorio;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await HistoricoRecursosRepositorio.PegarUltimasMovimentacoes());
+        }
+    }
+}
